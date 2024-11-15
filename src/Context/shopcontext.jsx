bfb814b2780c8d2af77369b45af16de13071cc48
Chaudaryabdullah <chaudaryabdullah899@gmail.com>
2024-11-15 01:46:09 -0800

    fixed

--- src/Context/shopcontext.jsx
+++ src/Context/shopcontext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState } from "react";
 import { products } from '../assets/assets';
 
-export const ShopContext = createContext();
+export  const ShopContext = createContext();
 
 
 const ShopContextProvider = (props) => {
